70a918b8da3765cd22fa08b1c35c3cbc36941548
yuu takagi <k024g0049@g.neec.ac.jp>
2025-04-17 18:15:57 +0900

    コメントアウト

--- main.cpp
+++ main.cpp
@@ -55,13 +55,17 @@ void Log(std::ostream &os, const std::string &message) {
   OutputDebugStringA(message.c_str());
 }
 
+//ウィンドウプロシージャ
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
-  switch (msg) {
+    //メッセージに応じてゲーム固有の処理を行う
+    switch (msg) {
+        //ウィンドウが破棄された
   case WM_DESTROY:
-
+      //OSに対して、アプリの終了を伝える
     PostQuitMessage(0);
     return 0;
   }
+    //標準のメッセージ処理を行う
   return DefWindowProc(hwnd, msg, wparam, lparam);
 }
 
@@ -82,80 +86,109 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
   std::ofstream logStream(logFilePath);
 
   WNDCLASS wc{};
+  //ウィンドウプロシージャ
   wc.lpfnWndProc = WindowProc;
+  //うウィンドウクラス名
   wc.lpszClassName = L"CG2WindowClass";
+  //インスタンスハンドル
   wc.hInstance = GetModuleHandle(nullptr);
+  //カーソル
   wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
 
+  //ウィンドウクラスを登録する
   RegisterClass(&wc);
 
+  //クライアント領域のサイズ
   const int32_t kClientWidth = 1280;
   const int32_t kClientheight = 720;
 
+  //ウィンドウサイズを表す構造体にクライアント領域を入れる
   RECT wrc = {0, 0, kClientWidth, kClientheight};
 
+  //クライアント領域を元に実際のサイズにwrcに変更してもらう
   AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
 
-  HWND hwnd =
-      CreateWindow(wc.lpszClassName, L"CG2", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT,
-                   CW_USEDEFAULT, wrc.right - wrc.left, wrc.bottom - wrc.top,
-                   nullptr, nullptr, wc.hInstance, nullptr);
+  //ウィンドウの生成
+  HWND hwnd = CreateWindow(
+      wc.lpszClassName,     //利用するクラス
+      L"CG2",               //タイトルバーの文字
+      WS_OVERLAPPEDWINDOW,  //よく見るウィンドウスタイル
+      CW_USEDEFAULT,        //表示X座標(Windowsに任せる)
+      CW_USEDEFAULT,        //表示Y座標(WindowOSに任せる)
+      wrc.right - wrc.left, //ウィンドウ横幅
+      wrc.bottom - wrc.top, //ウィンドウ縦幅
+      nullptr,              //親ウィンドウハンドル
+      nullptr,              //メニューハンドル
+      wc.hInstance,         //インスタンスハンドル
+      nullptr);             //オプション
 
+  //ウィンドウを表示する
   ShowWindow(hwnd, SW_SHOW);
 
+  //DXGIファクトリーの生成
   IDXGIFactory7* dxgiFactory=nullptr;
-
+  //HRESULTはWindows系エラーコードであり、
+  //関数が成功したかどうかをSUCCEEDEDマクロで判定できる
   HRESULT hr = CreateDXGIFactory(IID_PPV_ARGS(&dxgiFactory));
-
+  //初期化の根本的な部分でエラーが出た場合はプログラムが間違っているか
+  //どうにもできない場合が多いのでassertにしておく
   assert(SUCCEEDED(hr));
-
+  
+  //使用するアダプタ用の変数。最初にnullptrを入れておく
   IDXGIAdapter4 *useAdapter = nullptr;
 
+  //良い順にアダプタを頼む
   for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(
                        i, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE,
                        IID_PPV_ARGS(&useAdapter)) != DXGI_ERROR_NOT_FOUND;
        ++i) {
+      //アダプターの情報を取得する
     DXGI_ADAPTER_DESC3 adapterDesc{};
     hr = useAdapter->GetDesc3(&adapterDesc);
-    assert(SUCCEEDED(hr));
-
+    assert(SUCCEEDED(hr)); //取得できないのは一大事
+    //ソフトウェアアダプタ出なければ採用!
     if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE)) {
+      //採用したアダプタの情報をログに出力。wstringの方なので注意
       Log(logStream,ConvertString(std::format(L"Use Adapter:{}\n", adapterDesc.Description)));
       break;
     }
-    useAdapter = nullptr;
+    useAdapter = nullptr; //ソフトウェアアダプタの場合は見なかったことにする
   }
-
+  //適切なアダプタが見つからなっかったので起動できない
   assert(useAdapter != nullptr);
 
   ID3D12Device *device = nullptr;
-
+  //機能レベルとログ出力用の文字列
   D3D_FEATURE_LEVEL featureLevels[] = {
       D3D_FEATURE_LEVEL_12_2, D3D_FEATURE_LEVEL_12_1, D3D_FEATURE_LEVEL_12_0};
 
   const char *featureLevelStrings[] = {"12.2", "12.1", "12.0"};
-
+  //高い順に生成できるか試していく
   for (size_t i = 0; i < _countof(featureLevels); ++i) {
+    //採用したアダプターでデバイスを生成
     hr = D3D12CreateDevice(useAdapter, featureLevels[i], IID_PPV_ARGS(&device));
-
+    //指定した機能レベルでデバイスが生成できたかを確認
     if (SUCCEEDED(hr)) {
+      //生成できたのでログ出力を行ってループを抜ける
       Log(logStream, std::format("FeatureLevel : {}\n",
                                                featureLevelStrings[i]));
       break;
     }
   }
-
+  //デバイスの生成がうまくいかなかったので起動できない
   assert(device !=nullptr);
   Log(logStream, "Complete create D3D12Device!!!\n");
 
 
   MSG msg{};
+  //ウィンドウの×ボタンが押されるまでループ
   while (msg.message != WM_QUIT) {
+      //Windowにメッセージが来てたら最優先で処理させる
     if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
       TranslateMessage(&msg);
       DispatchMessage(&msg);
     } else {
-
+        //ゲームの処理
     }
   }
 
