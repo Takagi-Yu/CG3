d76a221cc32da25e604f2dec55e75a160d1ff203
yuu takagi <k024g0049@g.neec.ac.jp>
2025-04-23 16:11:13 +0900

    青い画面ができた

--- main.cpp
+++ main.cpp
@@ -259,36 +259,67 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
   // ディスクリプターヒープが作れなかったので起動できない
   assert(SUCCEEDED(hr));
 
+  // SwapChainからResourceを引っ張ってくる
   ID3D12Resource *swapChainResources[2] = {nullptr};
   hr = swapChain->GetBuffer(0, IID_PPV_ARGS(&swapChainResources[0]));
+  // うまく取得できなければ起動できない
   assert(SUCCEEDED(hr));
   hr = swapChain->GetBuffer(1, IID_PPV_ARGS(&swapChainResources[1]));
   assert(SUCCEEDED(hr));
 
+  // RTVの設定
   D3D12_RENDER_TARGET_VIEW_DESC rtvDesc{};
   rtvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
   rtvDesc.ViewDimension = D3D12_RTV_DIMENSION_TEXTURE2D;
+  // ディスクリプタの戦闘を取得する
   D3D12_CPU_DESCRIPTOR_HANDLE rtvStartHandle =
       rtvDescriptorHeap->GetCPUDescriptorHandleForHeapStart();
+  // RTVを2つ作るのでディスクリプタを2つ用意
   D3D12_CPU_DESCRIPTOR_HANDLE rtvHandles[2];
+  // まず1つ目を作る。1つ目は最初のところに作る。作る場所をこちらで指定してあげる必要がある
   rtvHandles[0] = rtvStartHandle;
   device->CreateRenderTargetView(swapChainResources[0], &rtvDesc,
                                  rtvHandles[0]);
+  // 2つ目のディスクリプタハンドルを得る(自力で)
   rtvHandles[1].ptr =
       rtvHandles[0].ptr +
       device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+  // 2つ目を作る
   device->CreateRenderTargetView(swapChainResources[1], &rtvDesc,
                                  rtvHandles[1]);
 
   MSG msg{};
-  //ウィンドウの×ボタンが押されるまでループ
+  // ウィンドウの×ボタンが押されるまでループ
   while (msg.message != WM_QUIT) {
-      //Windowにメッセージが来てたら最優先で処理させる
+      // Windowにメッセージが来てたら最優先で処理させる
     if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
       TranslateMessage(&msg);
       DispatchMessage(&msg);
     } else {
-        //ゲームの処理
+      // ゲームの処理
+      // これから書き込むバックバッファのインデックスを取得
+      UINT backBufferIndex = swapChain->GetCurrentBackBufferIndex();
+      // 描画先のRTVを設定する
+      commandList->OMSetRenderTargets(1, &rtvHandles[backBufferIndex], false,
+                                        nullptr);
+      // 指定した色で画面全体をクリアする
+      float clearColor[] = {0.1f, 0.25f, 0.5f, 1.0f};
+      commandList->ClearRenderTargetView(rtvHandles[backBufferIndex],
+                                           clearColor, 0, nullptr);
+      // コマンドリストの内容を確定させる。すべてのコマンドを積んでからCloseすること
+      hr = commandList->Close();
+      assert(SUCCEEDED(hr));
+
+      // GPUにコマンドリストの実行を行わせる
+      ID3D12CommandList *commandLists[] = {commandList};
+      commandQueue->ExecuteCommandLists(1, commandLists);
+      // GPUとOSに画面の交換を行うよう通知する
+      swapChain->Present(1, 0);
+      // 次のフレーム用のコマンドリストを準備
+      hr = commandAllocator->Reset();
+      assert(SUCCEEDED(hr));
+      hr = commandList->Reset(commandAllocator, nullptr);
+      assert(SUCCEEDED(hr));
     }
   }
 
